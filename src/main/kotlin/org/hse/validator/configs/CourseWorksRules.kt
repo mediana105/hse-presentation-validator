ae638329019d1cbeb61ca8c571a23096663a3663
mediana105 <dizalilova@edu.hse.ru>
2025-06-22 21:35:57 +0300

    feat: configs moved to separate configuration files

---
+++ src/main/kotlin/org/hse/validator/configs/CourseWorksRules.kt
@@ -0,0 +1,43 @@
+package org.hse.validator.configs
+
+import org.hse.validator.dsl.*
+
+val courseWorkConfig = presentationRules {
+    structure {
+        slidesCountRange = 10 to 15
+        requiredFormats(setOf("16:9", "4:3"))
+        mandatorySlides(listOf("Введение", "Заключение"))
+        limitColors = 3
+    }
+    slide {
+        style {
+            fonts {
+                sansSerifRequired = true
+                fontSizeRange(bodyMin = 18.0, bodyMax = 24.0, titleMin = 28.0, titleMax = 32.0)
+                maxFontStyle(maxBold = 10, maxItalic = 10, maxUnderline = 3)
+            }
+            limitFontVariety = 3
+            colors {
+                contrastThresholds(4.5, 3.0)
+            }
+        }
+        header {
+            requireHeaderFormat(10)
+        }
+        lists {
+            listSizeRange = 7
+            uniformCapitalizationRequired = true
+            singleItemsForbidden = true
+            endPunctuationForbidden = true
+        }
+        numbering {
+            slideNumberingRequired = true
+        }
+        graphics {
+            maxTextPercentage = 70
+        }
+        content {
+            limitTextPerSlide(maxLines = 10, maxWords = 40)
+        }
+    }
+}
--- src/main/kotlin/org/hse/validator/validators/profile/PresentationProfiles.kt
+++ src/main/kotlin/org/hse/validator/validators/profile/PresentationProfiles.kt
@@ -1,100 +1,16 @@
 package org.hse.validator.validators.profile
 
-import org.hse.validator.dsl.*
+import org.hse.validator.configs.courseWorkConfig
+import org.hse.validator.configs.finalQualificationWorkConfig
 import org.hse.validator.validators.rules.Rule
 import org.hse.validator.validators.rules.builder.RulesBuilder
 
+
 object PresentationProfiles {
     fun rulesFor(type: String): List<Rule> = when (type.lowercase()) {
-        "выпускная квалификационная работа (вкр)" -> finalQualificationWorkRules()
-        "курсовая работа" -> courseWorkRules()
+        "выпускная квалификационная работа (вкр)" -> RulesBuilder.build(finalQualificationWorkConfig)
+        "курсовая работа" -> RulesBuilder.build(courseWorkConfig)
         else -> throw IllegalArgumentException("Unknown type: $type")
     }
-
-    private fun finalQualificationWorkRules(): List<Rule> {
-        val config = presentationRules {
-            structure {
-                slidesCountRange = 10 to 18
-                requiredFormats(setOf("16:9", "4:3"))
-                mandatorySlides(listOf("Введение", "Заключение", "Список литературы"))
-                limitColors = 3
-            }
-            slide {
-                requireTitleSlideFields()
-                style {
-                    fonts {
-                        sansSerifRequired = true
-                        fontSizeRange(bodyMin = 18.0, bodyMax = 24.0, titleMin = 28.0, titleMax = 32.0)
-                        maxFontStyle(maxBold = 10, maxItalic = 10, maxUnderline = 3)
-                    }
-                    limitFontVariety = 3
-                    colors {
-                        contrastThresholds(4.5, 3.0)
-                    }
-                }
-                header {
-                    requireHeaderFormat(10)
-                }
-                lists {
-                    listSizeRange = 7
-                    uniformCapitalizationRequired = true
-                    singleItemsForbidden = true
-                    endPunctuationForbidden = true
-                }
-                numbering {
-                    slideNumberingRequired = true
-                }
-                graphics {
-                    maxTextPercentage = 70
-                }
-                content {
-                    limitTextPerSlide(maxLines = 10, maxWords = 40)
-                }
-            }
-        }
-        return RulesBuilder.build(config)
-    }
-
-    private fun courseWorkRules(): List<Rule> {
-        val config = presentationRules {
-            structure {
-                slidesCountRange = 10 to 15
-                requiredFormats(setOf("16:9", "4:3"))
-                mandatorySlides(listOf("Введение", "Заключение"))
-                limitColors = 3
-            }
-            slide {
-                style {
-                    fonts {
-                        sansSerifRequired = true
-                        fontSizeRange(bodyMin = 18.0, bodyMax = 24.0, titleMin = 28.0, titleMax = 32.0)
-                        maxFontStyle(maxBold = 10, maxItalic = 10, maxUnderline = 3)
-                    }
-                    limitFontVariety = 3
-                    colors {
-                        contrastThresholds(4.5, 3.0)
-                    }
-                }
-                header {
-                    requireHeaderFormat(10)
-                }
-                lists {
-                    listSizeRange = 7
-                    uniformCapitalizationRequired = true
-                    singleItemsForbidden = true
-                    endPunctuationForbidden = true
-                }
-                numbering {
-                    slideNumberingRequired = true
-                }
-                graphics {
-                    maxTextPercentage = 70
-                }
-                content {
-                    limitTextPerSlide(maxLines = 10, maxWords = 40)
-                }
-            }
-        }
-        return RulesBuilder.build(config)
-    }
 }
+
